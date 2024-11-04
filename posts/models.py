bf8f83663e771d90056bf31d0e89c485038170c8
osm3rr <osm3rr@gmail.com>
2024-11-04 17:01:36 -0400

    metodo __str__ incorporado a la clase Publication

--- posts/models.py
+++ posts/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Publication(models.Model):
     body = models.TextField()
+
+    def __str__(self) -> str:
+        return self.body[:50]
